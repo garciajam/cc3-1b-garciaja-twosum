9c04c0bf7f52afdbb1ab5e8d8738cd7211d16055
JOHN AARON GARCIA <jmg5819@students.uc-bcf.edu.ph>
2024-03-07 18:42:10 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace cc3_1b_garciaja_twosum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3,3 };
+            int target = 6;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        break;
+                    }
+                    if (nums[i] + nums[j] == target)
+                    {
+                        Console.WriteLine($"[ {i}, {j} ]");
+                    }                   
+                }
+            }
+
+
+            //foreach (int x in nums)
+            //{
+                
+                
+            //    foreach (int y in nums)
+            //    {
+            //        int index1 = Array.IndexOf(nums, x);
+            //        int index2 = Array.IndexOf(nums, y);
+
+            //        if (index1 == index2) 
+            //        {
+            //            break;
+            //        }   
+            //        else if (x + y == target)
+            //        {
+                        
+            //            Console.WriteLine($"[ {index1}, {index2} ]");
+                        
+            //        }  
+            //    }     
+            //}
+        }
+    }
+}
