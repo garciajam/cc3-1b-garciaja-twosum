6b4ed734e60834cff62bdd2f73868cab025b8726
JOHN AARON GARCIA <jmg5819@students.uc-bcf.edu.ph>
2024-03-07 22:47:16 +0800

    final

--- Program.cs
+++ Program.cs
@@ -7,17 +7,18 @@ namespace cc3_1b_garciaja_twosum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter amount of numbers in array: ");
+            Console.Write("Enter size of the array: ");
             int nos = Convert.ToInt32(Console.ReadLine());
             int[] nums = new int[nos];
 
             for (int x = 0; x < nos; x++)
             {
-                Console.WriteLine("Enter numbers: ");
+                Console.Write($"Enter index[{x}]: ");
                 nums[x] = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("Enter Target: ");
+            Console.Write("Enter Target: ");
             int target = Convert.ToInt32(Console.ReadLine());
+            bool solution = false;
 
             for (int i = 0; i < nums.Length; i++)
             {
@@ -29,9 +30,20 @@ namespace cc3_1b_garciaja_twosum
                     }
                     if (nums[i] + nums[j] == target)
                     {
-                        Console.WriteLine($"[ {i}, {j} ]");
-                    }                   
+                        Console.WriteLine($"Output: [ {j}, {i} ]");
+                        solution = true;
+                        break;
+                    }   
                 }
+
+                if (solution == true)
+                {
+                    break;
+                }
+            }
+            if (solution == false)
+            {
+                Console.WriteLine("No solution found!");
             }
         }
     }
